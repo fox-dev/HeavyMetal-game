156d196f12c59e4121cec9609a282ea5d64737a2
scott21493@gmail.com <scott21493@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-12 00:36:35 +0000

    Added base Ground Unit class

---
+++ project/UnitGround.java
@@ -0,0 +1,59 @@
+package project;
+
+public class UnitGround implements Unit{
+
+	@Override
+	public int getMoves() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setHP(int hp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean moved() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getAttack() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setLocationX(int x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLocationY(int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLocationX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLocationY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
