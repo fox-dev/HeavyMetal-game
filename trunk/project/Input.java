c95238d5cf311abd354bc698b2a5872c678a5c96
aabriam@gmail.com <aabriam@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-15 02:38:09 +0000

    Added a few more string checking for string inputs.  Still needs to check for valid coordinates of the players units.  Also changed it from 1 player object to two player objects: activePlayer and waitingPlayer.  Also added an attackCoordinates variable and its appropriate get function.

--- trunk/project/Input.java
+++ trunk/project/Input.java
@@ -18,21 +18,29 @@ public class Input {
 	/*firstCommand = do nothing, or coordinate of unit
 	 *secondCommand = cancel, or coordinates to move unit to
 	 *thirdCommand = action (attack, do nothing, cancel) 
+	 *attackCoordinates are need if the "thirdCommand" is an attack.
 	*/
-	private String firstCommand, secondCommand, thirdCommand;
-	Player thePlayer;
+	private String firstCommand, secondCommand, thirdCommand, attackCoordinates;
 	
-	public Input(Player p){
+	/*activePlayer = the player who has control of the board.
+	 *waitingPlayer = the player that is waiting.
+	 */
+	Player activePlayer, waitingPlayer;
+	
+	public Input(Player aP, Player wP){
 		firstCommand = "";
 		secondCommand = "";
 		thirdCommand = "";
-		thePlayer = p;
+		attackCoordinates = "";
+		activePlayer = aP;
+		waitingPlayer = wP;
 	}
 	
 	public Input(){
 		firstCommand = "";
 		secondCommand = "";
 		thirdCommand = "";
+		attackCoordinates = "";
 	}
 	
 	
@@ -44,14 +52,14 @@ public class Input {
 			System.out.println("Please enter your first command (Unit Coordinates, Cancel, or Do nothing)");
 			this.firstCommand = in.readLine();
 			System.out.println(firstCommand);
-			if(!(firstCommand.equalsIgnoreCase("Unit Coordinates") || firstCommand.equalsIgnoreCase("Cancel") || firstCommand.equalsIgnoreCase("Do nothing"))){
-				System.out.print("Your first command was not a valid input. ");
-				inputFirstCommand();
+			while(!(firstCommand.equalsIgnoreCase("Unit Coordinates") || firstCommand.equalsIgnoreCase("Cancel") || firstCommand.equalsIgnoreCase("Do nothing"))){
+				System.out.println("Your first command was not a valid input. Please enter your first command (Unit Coordinates, Cancel, or Do nothing)");
+				this.firstCommand = in.readLine();
 			}
-			else if(firstCommand.equalsIgnoreCase("Unit Coordinates")){ //Not fully implemented yet
+			if(firstCommand.equalsIgnoreCase("Unit Coordinates")){ //Not fully implemented yet
 				System.out.println("Checking unit location...");
-				if(thePlayer.getUnitAt(0, 0) == null){
-					System.out.println(thePlayer.getUnit(0).getLocationY());
+				if(activePlayer.getUnitAt(0, 0) == null){
+					System.out.println(activePlayer.getUnit(0).getLocationY());
 					System.out.print("You entered invalid coordinates. ");
 					inputFirstCommand();
 				}
@@ -90,7 +98,22 @@ public class Input {
 			System.out.println("Please enter your third command (Attack, Do nothing, Cancel)");
 			this.thirdCommand = in.readLine();
 			while(!(thirdCommand.equalsIgnoreCase("Attack") || thirdCommand.equalsIgnoreCase("Do nothing") || thirdCommand.equalsIgnoreCase("cancel"))){
+				System.out.println("Your third command was not a valid input. Please enter your third command (Attack, Do nothing, Cancel)");
 				this.thirdCommand = in.readLine();
+			}
+			if(thirdCommand.equalsIgnoreCase("Attack")){
+				System.out.println("Please enter the coordinates of an enemy unit to attack.");
+				attackCoordinates = in.readLine();
+				
+				if(waitingPlayer.getUnitAt(1, 0) == null){ //not fully implemented
+					System.out.print("The coordinates you entered for the enemy unit are invalid. ");
+					inputThirdCommand();
+				}
+				else{
+					System.out.println("Unit Coordinates are valid.");
+				}
+				
+				
 			}
 			System.out.println(thirdCommand);
 			
@@ -112,6 +135,11 @@ public class Input {
 	//get function for thirdCommand string
 	public String getThirdCommand(){
 		return thirdCommand;
+	}
+	
+	//get function for attackCoordinates string
+	public String getAttackCoordinates(){
+		return attackCoordinates;
 	}
 }
 
