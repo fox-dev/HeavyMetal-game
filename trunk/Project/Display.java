a071ea0db31ff651502e80fa33e59292414877ea
smoof31@hotmail.com <smoof31@hotmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-16 02:51:40 +0000

    Updated Display.

--- trunk/Project/Display.java
+++ trunk/Project/Display.java
@@ -1,19 +1,63 @@
 package project;
 
+import Proj.MapTest;
+import Proj.PlayerTest;
+
 //This file will display all maps/actions/units
 //And update after each action
 
-public class Display {
-	//Display the map and the unit locations
-	
-	public Display(Map map, Player player1, Player player2) {
-		/map
-		for(int i = 0; i < x; i++) {
-			for(int j = 0; j < y; j++) {
-				if(MapArra[x][y] == 2)
-					println('~~~')
-				else
-					println('___')
+public class DisplayTest {
+
+		private MapTest theMap;
+		private PlayerTest player1;
+		private PlayerTest player2;
+		private String[][] board;
+
+
+		//constructor
+		public DisplayTest(MapTest m, PlayerTest p1, PlayerTest p2){
+			theMap = m;
+			player1 = p1;
+			player2 = p2;
+			board = new String[m.getX()][m.getY()];
+			for(int x = 0; x < 20; x++){
+				for(int y = 0; y < 20; y++){
+					board[x][y] = Integer.toString(m.getArr(x, y)) + "  ";
+				}
+			}
+		}
 		
+		public void fillMap(){
+			for(int x = 0; x < theMap.getX(); x++){
+				for(int y = 0; y < theMap.getY(); y++){
+					if(player1.getUnitAt(x, y) != null){
+						board[y][x] = "[o]";
+					}
+					if(player2.getUnitAt(x, y) != null){
+						board[y][x] = "[x]";
+					}
+					else {//If getUnitAt is null
+						if(theMap.getArr(x, y) == 2 ){
+							board[y][x] = "~~~";
+						}
+						else{
+							board[y][x] = "---";
+						}
+					}
+				}
+			}
+		}
+			
+		public void printMap(){
+			fillMap();
+			for(int x = 0; x < theMap.getX(); x++){
+				for(int y = 0; y < theMap.getY(); y++){
+					System.out.print(board[x][y] + "  ");
+				}
+				System.out.println();
+				System.out.println();
+			}
+		}
+		
+
 	}
-}
