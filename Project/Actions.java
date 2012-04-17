b142dc8d9a471aa534c989c805c3b33757e18668
aviuswen@gmail.com <aviuswen@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-17 07:26:08 +0000

    small symmatic change from previous change

--- Project/Actions.java
+++ Project/Actions.java
@@ -128,7 +128,7 @@ public class Actions {
         if( isXY_onMap(xTemp,yTemp) ) //check if intermediate move is on map
           //add move if ( !(a ground unit and its on water) )
           if(!isGroundOnWater(u, xTemp,yTemp))
-            if(moveEl.numMovesLeft > 0)  //added by Dan Apr17 1218am
+            if(moveEl.numMovesLeft > 1)  //added by Dan Apr17 1218am
               q.enqueue(new MoveEl(moveEl.numMovesLeft-1, xTemp, yTemp, null));
       }
     }
