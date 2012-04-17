b2a6d31265bad90bb072e8c0a4cec792e8354345
aabriam@gmail.com <aabriam@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-17 04:51:43 +0000

    Changed the class around to handle integer inputs instead of string inputs for unit Coordinates, move Coordiantes, and attack Coordinates.  Added helper functions to hold the coordinates of the actions.  Added getter functions for in the input class to access individual X and Y coordinates of unit, move, and attack.  Still adding in the valid move checks.

--- trunk/Project/Input.java
+++ trunk/Project/Input.java
@@ -21,25 +21,33 @@ public class Input {
 	/*firstCommand = do nothing, or select unit coordinates
 	 *secondCommand = cancel, or move to a coordinate
 	 *thirdCommand = action (attack, do nothing, cancel) 
-	 *selectUnitCoordinates = coordinates of a selected unit (not always necessary)
-	 *moveCoordiantes = coordinates to move a selected unit to (not always necessary)
-	 *attackCoordinates are need if the "thirdCommand" is an attack. (not always necessary)
+	 *
+	 *The following have separate helper classes to hold the x and y integer values
+	 *unit_Coordinates = coordinates of a selected unit (not always necessary)
+	 *move_Coordinates = coordinates to move a selected unit to (not always necessary)
+	 *attack_Coordinates are need if the "thirdCommand" is an attack. (not always necessary)
 	*/
-	private String firstCommand, secondCommand, thirdCommand, selectUnitCoordinates, moveCoordinates, attackCoordinates;
+	private String firstCommand,secondCommand, thirdCommand;
 	
 	/*activePlayer = the player who has control of the board.
 	 *waitingPlayer = the player that is waiting.
 	 */
 	Player activePlayer, waitingPlayer;
 	
+	private unitCoordinates unit_Coordinates;
+	private moveCoordinates move_Coordinates;
+	private attackCoordinates attack_Coordinates;
+	
+	
 	public Input(Player aP, Player wP){
 		firstCommand = "";
 		secondCommand = "";
 		thirdCommand = "";
 		
-		selectUnitCoordinates = "";
-		moveCoordinates = "";
-		attackCoordinates = "";
+		unit_Coordinates = new unitCoordinates();
+		move_Coordinates = new moveCoordinates();
+		attack_Coordinates = new attackCoordinates();
+		
 		
 		activePlayer = aP;
 		waitingPlayer = wP;
@@ -50,13 +58,13 @@ public class Input {
 		secondCommand = "";
 		thirdCommand = "";
 		
-		selectUnitCoordinates = "";
-		moveCoordinates = "";
-		attackCoordinates = "";
+		unit_Coordinates = new unitCoordinates();
+		move_Coordinates = new moveCoordinates();
+		attack_Coordinates = new attackCoordinates();
 	}
 	
 	
-	//first Command
+	//first Command (This is the select unit command, if the player chooses Do nothing, their turn ends.)
 	public void inputFirstCommand(){
 		try{
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -69,11 +77,21 @@ public class Input {
 				this.firstCommand = in.readLine();
 			}
 			if(firstCommand.equalsIgnoreCase("Select Unit")){ //Not fully implemented yet
-				System.out.println("Please enter the coordinates of a friendly unit.");
-				selectUnitCoordinates = in.readLine();
+				try{
+					System.out.println("Please enter the X coordinate of a friendly unit.");
+					unit_Coordinates.setX(Integer.parseInt(in.readLine()));
+				
+					System.out.println("Please enter the Y coordinate of a friendly unit.");
+					unit_Coordinates.setY(Integer.parseInt(in.readLine()));
+					
+				}catch(NumberFormatException nfe){ 
+					System.out.println("Please enter digits only.");
+					unit_Coordinates.resetXY();
+					inputFirstCommand();
+				}
 				
 				System.out.println("Checking unit location...");
-				if(activePlayer.getUnitAt(10, 6) == null){
+				if(activePlayer.getUnitAt(unit_Coordinates.getX(), unit_Coordinates.getY()) == null){
 					System.out.print("You entered invalid coordinates. ");
 					inputFirstCommand();
 				}
@@ -88,28 +106,39 @@ public class Input {
 		
 	}
 	
-	//second Command
+	//second Command (This is the movement command, if user inputs Cancel, it should return to the firstCommand)
 	/*
 	 * If a unit is moved, the "moved" boolean of the unit should be set to true;
 	 * If "move to coordinate" is typed and the "moved" boolean is set to true, have function return and print that no further movements for that unit can be made.
+	 * If "Next" is inputed, the thirdCommand should be prompted, and the unit can still move after an attack.
 	 */
 	public void inputSecondCommand(){
 		try{
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 			
-			System.out.println("Please enter your second command (Move to Coordinate, or Cancel)");
+			System.out.println("Please enter your second command (Move to Coordinate, Next, or Cancel)");
 			this.secondCommand = in.readLine();
 		
-			while(!(secondCommand.equalsIgnoreCase("Move to Coordinate") || secondCommand.equalsIgnoreCase("Cancel"))){
-				System.out.println("Your second command was not a valid input. Please enter your second command (The coordinates to move your unit to, or Cancel)");
+			while(!(secondCommand.equalsIgnoreCase("Move to Coordinate") || secondCommand.equalsIgnoreCase("Next") || secondCommand.equalsIgnoreCase("Cancel"))){
+				System.out.println("Your second command was not a valid input. Please enter your second command (Move to Coordinate, Next, or Cancel)");
 				this.secondCommand = in.readLine();
 			}
 			
 			if(secondCommand.equalsIgnoreCase("Move to Coordinate")){
 				
-				System.out.println("Please enter the coordinates you want to the selected unit");
-				this.moveCoordinates = in.readLine();
+				try{
+					System.out.println("Please enter the X coordinate of the location to move the selected unit");
+					move_Coordinates.setX(Integer.parseInt(in.readLine()));
 				
+					System.out.println("Please enter the Y coordinate of the location to move the selected unit");
+					move_Coordinates.setY(Integer.parseInt(in.readLine()));
+				}catch(NumberFormatException nfe){
+					System.out.println("Please enter digits only.");
+					move_Coordinates.resetXY();
+					inputSecondCommand();
+				}
+				
+		
 				if(false){ //Need to check if a unit can make that move
 					System.out.println("You cannot make that move.");
 					inputSecondCommand();
@@ -123,7 +152,10 @@ public class Input {
 		
 	}
 	
-	//third Command
+	//third Command (This is the attack command)
+	/*This command should be prompted after the second command, if do nothing is inputed, this ends the movement of the selected unit.
+	 * If cancel is selected, the second Command should be re-prompted to the user.
+	 */
 	public void inputThirdCommand(){
 		try{
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -135,10 +167,20 @@ public class Input {
 				this.thirdCommand = in.readLine();
 			}
 			if(thirdCommand.equalsIgnoreCase("Attack")){
-				System.out.println("Please enter the coordinates of an enemy unit to attack.");
-				this.attackCoordinates = in.readLine();
+				try{
+					System.out.println("Please enter the X coordinate of an enemy unit to attack.");
+					attack_Coordinates.setX(Integer.parseInt(in.readLine()));
 				
-				if(waitingPlayer.getUnitAt(1, 0) == null){//not fully implemented
+					System.out.println("Please enter the Y coordinate of an enemy unit to attack.");
+					attack_Coordinates.setY(Integer.parseInt(in.readLine()));
+				}catch(NumberFormatException nfe){
+					System.out.println("Please enter digits only.");
+					attack_Coordinates.resetXY();
+					inputThirdCommand();
+				}
+				
+				
+				if(waitingPlayer.getUnitAt(attack_Coordinates.getX(), attack_Coordinates.getY()) == null){//not fully implemented
 					System.out.print("The coordinates you entered for the enemy unit are invalid. ");
 					inputThirdCommand();
 				}
@@ -169,19 +211,39 @@ public class Input {
 		return thirdCommand;
 	}
 	
+	/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	 * the Following functions are COORDINATE getter functions
+	 * for X and Y coordinates.  These methods can be called
+	 * by the Input class without having to reference
+	 * the helper classes in other classes.
+	 *XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
 	//get function for the selected unit's coordinates
-	public String getSelectUnitCoordinates(){
-		return selectUnitCoordinates;
+	public int getUnitCoordinates_X(){
+		return unit_Coordinates.getX();
 	}
 	//get function for move coordinates
-	public String getMoveCoordinates(){
-		return moveCoordinates;
+	public int getMoveCoordinates_X(){
+		return move_Coordinates.getX();
 	}
 	
 	//get function for attackCoordinates string
-	public String getAttackCoordinates(){
-		return attackCoordinates;
+	public int getAttackCoordinates_X(){
+		return attack_Coordinates.getX();
 	}
+	/////////////////////////////////////////////////////////////
+	public int getUnitCoordinates_Y(){
+		return unit_Coordinates.getY();
+	}
+	//get function for move coordinates
+	public int getMoveCoordinates_Y(){
+		return move_Coordinates.getY();
+	}
+	
+	//get function for attackCoordinates string
+	public int getAttackCoordinates_Y(){
+		return attack_Coordinates.getY();
+	}
+	/////////////////////////////////////////////////////////////
 	
 	//function for switching the activePlayer with the waitingPlayer, used when activePlayer turn ends.
 	public void switchPlayerStatuses(){
@@ -192,10 +254,107 @@ public class Input {
 		firstCommand = "";
 		secondCommand = "";
 		thirdCommand = "";
-		
-		selectUnitCoordinates = "";
-		moveCoordinates = "";
-		attackCoordinates = "";
 	}
+}
+
+/*XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+ *  Helper Classes for Input Class
+ * These classes are basically for keeping track of the following:
+ * -Selected Unit Coordinates
+ * -Movement Coordinates
+ * -Attack Coordinates
+ * Note: Default values are set to -1 since -1 is off the map and java
+ * does not accept null ints.
+ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX*/
+
+class unitCoordinates{
+	private int x;
+	private int y;
+	
+	public unitCoordinates(){
+		
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void resetXY(){
+		x = -1;
+		y = -1;
+	}
+}
+
+class moveCoordinates{
+	private int x;
+	private int y;
+	
+	public moveCoordinates(){
+		x = -1;
+		y = -1;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void resetXY(){
+		x = -1;
+		y = -1;
+	}
+	
+}
+
+class attackCoordinates{
+	private int x;
+	private int y;
+	
+	public attackCoordinates(){
+		x = -1;
+		y = -1;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void resetXY(){
+		x = -1;
+		y = -1;
+	}
+	
 }
 
