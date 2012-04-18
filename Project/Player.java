0ad210eb455962ce63637fc09530d7f52addacf9
aviuswen@gmail.com <aviuswen@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-18 08:10:41 +0000

    Player(int playerNum) updated to use function calls (Unit).setXY(int X, int Y) Default values for private static final for players1 and player2 to help prevent overlap New constructor Player(int airX, int airY, int groundX, int groundY) for testing purposes Player.java ArrayList<Unit> units = new ArrayList<Unit>(); changed back to private

--- Project/Player.java
+++ Project/Player.java
@@ -3,91 +3,109 @@ package project;
 import java.util.ArrayList;
 
 public class Player {
-	// Creates the units
-	// Keep track of active units
-	// How many units that have been moved during the turn
+  //default coordinates
+  //Created so that units will not be atop each other and to have them
+  //located in one area.  REMAKE LATER INTO FINAL INT[][]
+  private static final int P1_AIR_X = 4;      // ( 4, 5)
+  private static final int P1_AIR_Y = 5;
+  private static final int P1_GROUND_X = 4;   // ( 4,10)
+  private static final int P1_GROUND_Y = 10;
+  private static final int P2_AIR_X = 11;     // (11, 5)
+  private static final int P2_AIR_Y = 5;
+  private static final int P2_GROUND_X = 12;  // (12, 5)
+  private static final int P2_GROUND_Y = 5;
+  
+  // Creates the units
+  // Keep track of active units
+  // How many units that have been moved during the turn
 
   //Changed to package class access because of a lack of methods to access certian portions of data members DAN
-	ArrayList<Unit> units = new ArrayList<Unit>();
+  private ArrayList<Unit> units = new ArrayList<Unit>();
 
-	private UnitAir air;
-	private UnitGround ground;
+  private UnitAir air;
+  private UnitGround ground;
 
-	//Constructor, creates units, sets their location
-	public Player(int playNum) {
-		
-		air = new UnitAir();
-		ground = new UnitGround();
-		
-		if(playNum == 1) {
-			air.setLocationX(4);
-			air.setLocationY(5);
-			ground.setLocationX(4);
-			ground.setLocationY(10);
-		}
-		if(playNum == 2) {
-			ground.setLocationX(12);
-			ground.setLocationY(5);
-			air.setLocationX(11);
-			air.setLocationY(5);
-		}
-		
-		units.add(air);
-		units.add(ground);
-	}
-
-	//Removes units that have no more HP
-	public void removeDeadUnits() {
-		for (int i = 0; i < units.size(); i++) {
-			Unit tempUnit = units.get(i);
-			if(tempUnit.getHP() <= 0) {
-				units.remove(i);
-				i--;
-			}
-		}
-	}
-	
-	//Checks to see if the player's turn is over
-	public boolean checkTurnOver() {
-		for (int i = 0; i < units.size(); i++) {
+  //Constructor, creates units, sets their location
+  public Player(int playNum) {
+    
+    air = new UnitAir();
+    ground = new UnitGround();
+    
+    if(playNum == 1) {
+      air.setXY(P1_AIR_X, P1_AIR_Y);
+      ground.setXY(P1_GROUND_X, P1_GROUND_Y);
+    }
+    if(playNum == 2) {
+      air.setXY(P2_AIR_X, P2_AIR_Y);
+      ground.setXY(P2_GROUND_X, P2_GROUND_Y);
+    }
+    
+    units.add(air);
+    units.add(ground);
+  }
+  
+  //Added by DAN
+  public Player(int airX, int airY, int groundX, int groundY){
+    air = new UnitAir();
+    ground = new UnitGround();
+    air.setXY(airX, airY);
+    ground.setXY(groundX, groundY);
+    units.add(air);
+    units.add(ground);    
+  }
+  
+  //Removes units that have no more HP
+  public void removeDeadUnits() {
+    for (int i = 0; i < units.size(); i++) {
+      Unit tempUnit = units.get(i);
+      if(tempUnit.getHP() <= 0) {
+        units.remove(i);
+        i--;
+      }
+    }
+  }
+  
+  //Checks to see if the player's turn is over
+  public boolean checkTurnOver() {
+    for (int i = 0; i < units.size(); i++) {
       if((units.get(i)).hasMoved() == false)
-				return false;
-		}
-		
-		return true;
-	}
-	
-	//Returns a unit at a position x, y or null if there is nothing at that position
-	public Unit getUnitAt(int x, int y) {
-		for(int i = 0; i < units.size(); i++) {
-			if(units.get(i).getLocationX() == x && units.get(i).getLocationY() == y) {
-				return units.get(i);
-			}
-		}
-		
-		return null;
-	}
-	
-	//Gets a unit called by its index
-	public Unit getUnit(int unitNum) {
-		if(unitNum > units.size() - 1){
-			return null;
-		}
-		return units.get(unitNum);
-	}
-	
-	//Checks how many active units there are
-	public int checkNumUnits() {
-		return units.size();
-	}
-	
-	//Reset all units of Player to Unit.moved == false Dan
-	//Reset all units of Player to Unit.setHasUnitShot = false Dan
-	public void unitsReset(){
+        return false;
+    }
+    
+    return true;
+  }
+  
+  //Returns a unit at a position x, y or null if there is nothing at that position
+  public Unit getUnitAt(int x, int y) {
+    for(int i = 0; i < units.size(); i++) {
+      if(units.get(i).getLocationX() == x && units.get(i).getLocationY() == y) {
+        return units.get(i);
+      }
+    }
+    
+    return null;
+  }
+  
+  //Gets a unit called by its index
+  public Unit getUnit(int unitNum) {
+    if(unitNum > units.size() - 1){
+      return null;
+    }
+    return units.get(unitNum);
+  }
+  
+  //Checks how many active units there are
+  public int checkNumUnits() {
+    return units.size();
+  }
+  
+  //Reset all units of Player to Unit.moved == false Dan
+  //Reset all units of Player to Unit.setHasUnitShot = false Dan
+  public void unitsReset(){
     for (int i = 0; i < units.size(); i++){
       units.get(i).movedFalse();
       units.get(i).setHasUnitShot(false);
     }
-	}
+  }
 }
 
