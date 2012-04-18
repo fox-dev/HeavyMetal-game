e9a3d94e51a8bfd434164a9df231f60ea8da230f
aviuswen@gmail.com <aviuswen@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-18 07:58:39 +0000

    Unit.java was changed to a class.  UnitGround will inherit methods and data members from Unit because methods and data members are the same types and method signatures are exactly the same.  Defaults for UnitGround have been changed to static finals to add visibility.  Constructors also call those from superclass Unit with those static final defaults.

--- trunk/Project/UnitGround.java
+++ trunk/Project/UnitGround.java
@@ -1,5 +1,5 @@
 // UnitGround.java
-// Implements Unit.java
+// Implements Unit.java //changed to inherit DAN
 //
 // A generic ground unit will have the following:
 // HP = 4
@@ -16,73 +16,19 @@
 
 package project;
 
-public class UnitGround implements Unit {
-
-	private int HP;
-	private int numMoves;
-	private int attack;
-	private int locX;
-	private int locY;
-	private boolean moved = false;
-	private boolean hasUnitShot = false;   //Dan: ensures that unit can only shoot once
-	
+public class UnitGround extends Unit {
+  //defaults for UnitGround DAN 
+  private static final int HP_DEFAULT = 4;
+  private static final int MOVES_DEFAULT = 2;
+  private static final int ATK_DEFAULT = 2;
+  private static final int SPAWN_LOC_X = 0;
+  private static final int SPAWN_LOC_Y = 0;
+  
 	public UnitGround() {
-		HP = 4;
-		numMoves = 2;
-		attack = 2;
-		locX = 0;
-		locY = 0;
+    super(HP_DEFAULT,MOVES_DEFAULT,ATK_DEFAULT,SPAWN_LOC_X,SPAWN_LOC_Y);
 	}
 	
 	public UnitGround(int setHP, int setNumMoves, int ATK, int locationX, int locationY) {
-		HP = setHP;
-		numMoves = setNumMoves;
-		attack = ATK;
-		locX = locationX;
-		locY = locationY;
+    super(setHP, setNumMoves, ATK, locationX, locationY);
 	}
-	public int getMoves() {
-		return numMoves;
-	}
-	public int getHP() {
-		return HP;
-	}
-	public void setHP(int setHP) {
-		HP = setHP;
-	}
-	public void moved() {
-		moved = true;
-	}
-	public void movedFalse() { //added by Dan
-	  moved = false;
-	}
-	public boolean hasMoved() {
-		return moved;
-	}
-	public int getAttack() {
-		return attack;
-	}
-	public void setLocationX(int x) {
-		locX = x;
-	}
-	public void setLocationY(int y) {
-		locY = y;
-	}
-	public int getLocationX() {
-		return locX;
-	}	
-	public int getLocationY() {
-		return locY;
-	}
-  public void setHasUnitShot(boolean b){
-    hasUnitShot = b;
-  }
-  
-  public boolean getHasUnitShot(){
-    return hasUnitShot;
-  }
-  public void setXY(int x, int y){ //added by DAN for convenience
-    setLocationX(x);
-    setLocationY(y);
-  }
 }
