7d9a6050b5a696c75c417c528416ecc2469942e5
aviuswen@gmail.com <aviuswen@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-18 09:05:18 +0000

    Map.java static final added for defaults of x and y Map.java public Map(Map m) "copy constructor" made for troubleshooting and test casing Map.java public Map() changed to call public Map(int xsize, int ysize) constructor with static final defaults Map.java private void copyMap( Map target ) added for convenience

--- Project/Map.java
+++ Project/Map.java
@@ -11,48 +11,55 @@
  * 2 = Water
  */
 
-
 public class Map {
-	private int[][] mapArr; // 2D Map array
-	private int x; // Size of X
-	private int y; // Size of Y
-	
-	public Map() {
-		x = 20; // Set default X
-		y = 20; // Set default Y
-		mapArr = new int[x][y];
-		// double rand = 0;
-		for(int i = 0; i < x; i++)
-			for(int j = 0; j < y; j++) {
-				/*
-				rand = Math.random();
-				if (rand >= 0.8)
-					mapArr[i][j] = 2;
-				else
-					mapArr[i][j] = 1;
-				*/
-				mapArr[i][j] = 1;
-			}
-	}
-	// Specify a custom X and Y and make array that size
-	public Map(int xsize, int ysize) {
-		x = xsize;
-		y = ysize;
-		mapArr = new int[x][y];
-		// double rand = 0;
-		for(int i = 0; i < x; i++)
-			for(int j = 0; j < y; j++) {
-				/*
-				rand = Math.random();
-				if (rand >= 0.8)
-					mapArr[i][j] = 2;
-				else
-					mapArr[i][j] = 1;
-				*/
-				mapArr[i][j] = 1;
-			}
-	}
-	public int getX() { return x; } // Returns X size
-	public int getY() { return y; } // Returns y size
-	public int getArr(int x, int y) { return mapArr[x][y]; } // Returns # at given coordinates
+  private static final int DEFAULT_SIZE_X = 20;
+  private static final int DEFAULT_SIZE_Y = 20;
+  
+  private int[][] mapArr; // 2D Map array
+  private int x; // Size of X
+  private int y; // Size of Y
+  
+  public Map() {
+    this(DEFAULT_SIZE_X, DEFAULT_SIZE_Y);
+  }
+  
+  // Specify a custom X and Y and make array that size
+  public Map(int xsize, int ysize) {
+    x = xsize;
+    y = ysize;
+    mapArr = new int[x][y];
+    // double rand = 0;
+    for(int i = 0; i < x; i++)
+      for(int j = 0; j < y; j++) {
+        /*
+        rand = Math.random();
+        if (rand >= 0.8)
+          mapArr[i][j] = 2;
+        else
+          mapArr[i][j] = 1;
+        */
+        mapArr[i][j] = 1;
+      }
+  }
+  public int getX() { return x; } // Returns X size
+  public int getY() { return y; } // Returns y size
+  public int getArr(int x, int y) { return mapArr[x][y]; } // Returns # at given coordinates
+   
+  //Create a new map from a previous map.  Made for testing cases and troubleshooting DAN
+  public Map(Map m){ 
+    //create a Map object using the constructors
+    this(m.getX(),m.getY());
+    //copy contents
+    m.copyMap(this);
+  }
+  
+  private void copyMap(Map target){
+    //MAKES NO CHECK FOR SIZE, HENCE MADE PRIVATE FOR "COPY" CONSTRUCTOR DAN
+    for(int i = 0; i < target.mapArr.length; i++) {
+      for(int j = 0; j < target.mapArr[i].length; j++){
+        this.mapArr[i][j] = target.mapArr[i][j]; 
+      }
+    }    
+    
+  }
 }
