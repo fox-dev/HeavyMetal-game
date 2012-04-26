0b1b94be5c6b9c022078b5b0ae56d7ffae4cbfaf
aabriam@gmail.com <aabriam@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-04-26 03:22:31 +0000

    added mouseMoved function to keep track of the mouse's X and Y locations on the map.  Also added get functions for these mouseMoved variables.  Also added get function for selectedUnit from input.

--- Project/Input.java
+++ Project/Input.java
@@ -81,9 +81,10 @@ public class Input {
 		
 		
 		//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-		//          GET FUNCTIONS
+		//           GET FUNCTIONS
+		//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 		//Get function for the location mouseX
-		public int getMouseXX(){
+		public int getMouseX(){
 			return mouseX;
 		}
 		
