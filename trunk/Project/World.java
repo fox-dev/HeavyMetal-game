eb92368af77c8fe7768ac1ca88e50774eb0d8977
fmartinez255@gmail.com <fmartinez255@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-05-07 23:32:46 +0000

    Map.java: - Added new constructor Map(int,int,boolean). Makes a map with or without water with default side chances - Changed structure of isMapValid a bit. It should be stricter now - Fixed flipped coordinates - Fixed exported maps having flipped coordinates - Added setArr(int x, int y, tin tileValue). This method sets the tile at (x,y) to tileValue
    World.java:
    - Rectangle and Image arrays use map dimensions instead of MAP_DEM
    - Swapped y = (j+1)*TILE_SIZE with x = (i+1)*TILE_SIZE to fix flipped coordinate problem
    
    Drawing.java:
    - Swapped the for statements constraints. This fixes an out of bounds array exception in a non-square map
    
    Actions.java:
    - Added a check to moveLegal if a mouse click is outside of the map. It  originally threw an array out of bounds exception
    - getArr() method calls have been swapped to (x,y) instead of (y,x)

--- trunk/Project/World.java
+++ trunk/Project/World.java
@@ -11,6 +11,7 @@ public class World {
 	private Image[][] tileImg;
 	Map map;
 	public static final int TILE_SIZE = 30;
+	// We're not using MAP_DEM at this moment
 	public static final int MAP_DEM = 20;
 	private int x= 0, y = 0;
 	
@@ -20,37 +21,45 @@ public class World {
 	public World() {
 		TILE_GRASS = new ImageIcon("images/grass.png").getImage();
 		TILE_WATER = new ImageIcon("images/water.png").getImage();
-		tiles = new Rectangle[MAP_DEM][MAP_DEM];
-		tileImg = new Image[MAP_DEM][MAP_DEM];
+		// Map is now created before tiles and tileImg so we can get its dimensions
 		map = new Map();
+		// The rectangles and images now get the dimensions of the map
+		// This makes non-square map dimensions like (20,20) or (15,15) possible
+		// without things crashing and burning
+		tiles = new Rectangle[map.getX()][map.getY()];
+		tileImg = new Image[map.getX()][map.getY()];
 		loadArrays();
 	}
 
 	
 	//Take the map and fill in a 2d array with images for drawing
 	private void loadArrays() {
-		for(int i = 0; i < MAP_DEM; i++) {
-			for (int j = 0; j < MAP_DEM; j++){
+		for(int i = 0; i < map.getX(); i++) {
+			for (int j = 0; j < map.getY(); j++){
+				// For testing purposes
+				//System.out.println("Y = " + i + " X = " + j + " MAPX = " + map.getX() + " MAPY = " + map.getY());
 				if(map.getArr(i, j) == 1) {
 					tileImg[i][j] = TILE_GRASS;
 				}
 				else if(map.getArr(i, j) == 2) {
 					tileImg[i][j] = TILE_WATER;
 				}
+				else
+					;
 				
 				tiles[i][j] = new Rectangle(x, y, TILE_SIZE, TILE_SIZE);
 				
-				x = (j+1)*TILE_SIZE;
+				y = (j+1)*TILE_SIZE;
 			}
-			x = 0;
-			y = (i+1) *TILE_SIZE;
+			y = 0;
+			x = (i+1)*TILE_SIZE;
 		}	
 	}
 	
 	//Draw each tile to the screen
 	public void draw(Graphics g) {
-		for(int i = 0; i < MAP_DEM; i++) {
-			for(int j = 0; j < MAP_DEM; j++) {
+		for(int i = 0; i < map.getX(); i++) {
+			for(int j = 0; j < map.getY(); j++) {
 			g.drawImage(tileImg[i][j], tiles[i][j].x, tiles[i][j].y, null);
 			}
 		}
