33443cabfce8945e5755c18438e3ad651fa7a2c9
a.abriam@gmail.com <a.abriam@gmail.com@3bd22829-0006-a5d6-911f-40a801900771>
2012-06-03 04:42:06 +0000

    I added currentUnit.attacked();in unitsReset to fix that base issue with switching players when the base has enough points to make more units, if other problems occur, remove that line.

--- trunk/Project/Player.java
+++ trunk/Project/Player.java
@@ -137,6 +137,7 @@ public class Player {
       currentUnit.setHasUnitShot(false);
       if(currentUnit instanceof UnitBase){
     	  currentUnit.movedFalse();
+        currentUnit.attacked();
       }
     }
   }
